423f07b508a8193961e12b2ef5ad177f55958881
alexbrks <alex_brks@hotmail.com>
2016-04-11 09:59:29 +0200

    Spooky Skeleton

---
+++ src/main/java/es/uniovi/asw/electioralManagementSys/ElectionParameters.java
@@ -0,0 +1,12 @@
+package es.uniovi.asw.electioralManagementSys;
+
+import java.sql.Date;
+import java.util.List;
+
+import es.uniovi.asw.pollingst.PollingStation;
+
+public class ElectionParameters {
+	private Date electionDate;
+	private List<PollingStation> pollingStations;
+	private List<PoliticalParty> parties;
+}
