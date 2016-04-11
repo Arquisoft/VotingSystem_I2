7fea5cd8b8b69647eaa422da80492816ede790e9
alexbrks <alex_brks@hotmail.com>
2016-04-11 09:06:11 +0200

    Dependency inversion

---
+++ src/main/java/es/uniovi/asw/db/MockDB.java
@@ -0,0 +1,29 @@
+package es.uniovi.asw.db;
+
+public class MockDB implements Repository {
+
+	@Override
+	public void electoralDataStorage() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadElectionParameters() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getVoter() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertVote() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/es/uniovi/asw/db/Repository.java
@@ -0,0 +1,10 @@
+package es.uniovi.asw.db;
+
+public interface Repository {
+	
+	void electoralDataStorage();
+	void loadElectionParameters();
+	void getVoter();
+	void insertVote();
+
+}
